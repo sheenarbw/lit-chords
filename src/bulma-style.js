f4aa4f73e242430e4a3b51ebe341171d860f07cc
Sheena <sheena.oconnell@gmail.com>
2021-11-21 16:15:31 +0200

    sharps calculated correctly

---
+++ src/bulma-style.js
@@ -0,0 +1,6 @@
+import { html } from 'lit';
+
+export const bulmaStyle = html`<link
+  rel="stylesheet"
+  href="https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css"
+/>`;
--- src/lit-chord.js
+++ src/lit-chord.js
@@ -1,86 +1,116 @@
-import { LitElement, html, } from 'lit';
+import { LitElement, html } from 'lit';
+import { bulmaStyle } from './bulma-style';
 
 const chordProgressions = [
-    [1,5,6,4],
-    [1,6,2,5],
-    [1,6,4,5],
-    [1,6,3,5],
-    [1,3,4,5],
-    [1,5,4,5],
-    [1,5,2,4],
-    [3,6,2,5],
-    [6,4,2,5],
-    [6,4,5,1],
-    [1,4,1,5],
-]
+  [1, 5, 6, 4],
+  [1, 6, 2, 5],
+  [1, 6, 4, 5],
+  [1, 6, 3, 5],
+  [1, 3, 4, 5],
+  [1, 5, 4, 5],
+  [1, 5, 2, 4],
+  [3, 6, 2, 5],
+  [6, 4, 2, 5],
+  [6, 4, 5, 1],
+  [1, 4, 1, 5],
+];
 
-const MAJOR = "major"
-const MINOR = "minor"
-const DIMINISHED = "diminished"
+const MAJOR = '';
+const MINOR = 'm';
+const DIMINISHED = 'diminished';
 
-const rootNotes = "ABCDEFG"
-const chords = [MAJOR,MINOR,MINOR,MAJOR,MAJOR,MINOR,DIMINISHED]
+const rootNotes = 'ABCDEFG';
+const noteCircle = [
+  'A',
+  'A#',
+  'B',
+  'C',
+  'C#',
+  'D',
+  'D#',
+  'E',
+  'F',
+  'F#',
+  'G',
+  'G#',
+];
 
-const randomChoice = (array) => array[Math.floor(Math.random() * array.length)];
+const chords = [MAJOR, MINOR, MINOR, MAJOR, MAJOR, MINOR, DIMINISHED];
 
+const randomChoice = array => array[Math.floor(Math.random() * array.length)];
 
 export class LitChord extends LitElement {
-
-    constructor(){
-        super()
-        this.randomize()
-    }
-
-  properties(){
-      return {
-          rootNote: {type:String},
-          progression: {type: Array}
-      }
+  constructor() {
+    super();
+    this.randomize();
   }
 
-  randomize(){
-      this.rootNote = randomChoice(rootNotes);
-      this.progression = randomChoice(chordProgressions);
-
+  // eslint-disable-next-line class-methods-use-this
+  properties() {
+    return {
+      rootNote: { type: String },
+      progression: { type: Array },
+    };
   }
 
-  progressionNumerals(){
-    const romanNumerals = [
-        'I','II','III','IV','V','VI','VII'
-    ]
-    return this.progression.map(num=>romanNumerals[num-1]).join(' - ')
+  randomize() {
+    this.rootNote = randomChoice(rootNotes);
+    this.progression = randomChoice(chordProgressions);
   }
 
-  progressionChords(){
-    const rootIndex = rootNotes.indexOf(this.rootNote)
-    const notes = rootNotes.slice(rootIndex) + rootNotes.slice(0,rootIndex)
+  progressionNumerals() {
+    const romanNumerals = ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII'];
+    return this.progression.map(num => romanNumerals[num - 1]);
+  }
 
-    return this.progression.map(num => `${notes[num-1]} ${chords[num-1]}`).join(' - ')
+  notesInScale() {
+    const majorScale = [2, 2, 1, 2, 2, 2, 1];
+
+    const rootNoteIndex = noteCircle.indexOf(this.rootNote);
+    const noteIndices = [rootNoteIndex];
+    majorScale.forEach(num => {
+      noteIndices.push(num + noteIndices[noteIndices.length - 1]);
+    });
+    const notes = noteIndices.map(
+      index => noteCircle[index % noteCircle.length]
+    );
+
+    return notes;
+  }
+
+  progressionChords() {
+    // const rootIndex = rootNotes.indexOf(this.rootNote);
+    // const notes = rootNotes.slice(rootIndex) + rootNotes.slice(0, rootIndex);
+    const notes = this.notesInScale();
+    return this.progression.map(num => `${notes[num - 1]}${chords[num - 1]}`);
   }
 
   render() {
+    const numerals = this.progressionNumerals();
+    const selectedChords = this.progressionChords();
+    const notes = this.notesInScale();
 
     return html`
-        <h1>Lit chord</h1>
-        <h2>Key: ${this.rootNote}</h2>
-        <h2>Chords: </h2>
-        <p>
-            ${this.progressionNumerals()}
-        </p>
-        <p>
-            ${this.progressionChords()}
-        </p>
+      <div class="block box">
+        <h1 class="title">Lit chord</h1>
+        <h2 class="subtitle">Key: ${this.rootNote}</h2>
 
-        <ul>
-            <li>Triad practice</li>
-            <li>Reactive listening</li>
-            <li>Scales</li>
-            <li>Arpeggios</li>
-        </ul>
+        <p><strong>Notes</strong> ${notes.join(' ')}</p>
 
+        <div class="columns is-mobile">
+          ${numerals.map(
+            (element, index) => html`
+              <div class="column">
+                <h3 class="title is-3">${element}</h3>
+                <h3 class="title is-3">${selectedChords[index]}</h3>
+              </div>
+            `
+          )}
+        </div>
+      </div>
+      ${bulmaStyle}
     `;
   }
 }
-
 
 customElements.define('lit-chord', LitChord);
